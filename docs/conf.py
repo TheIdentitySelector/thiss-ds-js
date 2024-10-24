7685696b9cab14ebedd966ae4b1bab9e48647fbb
enriquepablo <enrique@cazalla.net>
2024-10-24 17:44:19 +0200

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Leif Johansson'
 # The short X.Y version
 version = u'2.1'
 # The full version, including alpha/beta/rc tags
-release = u'2.1.0'
+release = u'2.1.1'
 
 js_source_path = '../src'
 
--- docs/releasenotes.rst
+++ docs/releasenotes.rst
@@ -5,3 +5,8 @@ Version 1.0.13
 -------------
 
 * Support for turning off persistence in DiscoveryService.do_saml_discovery_response
+
+Version 2.1.1
+-------------
+
+* Support for Storage Access API
