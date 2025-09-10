dbcd4cc54276f8bb6d5056a19eaadc04586885d4
enriquepablo <enrique@cazalla.net>
2025-09-10 14:19:38 +0200

    bump to 3.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Leif Johansson'
 # The short X.Y version
 version = u'3.0'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.0'
+release = u'3.0.1'
 
 js_source_path = '../src'
 
